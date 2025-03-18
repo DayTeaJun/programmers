72a0a3a5c7bcd16bec8e2d6162d4befff39830d1
JunYoung <108723143+DayTeaJun@users.noreply.github.com>
2025-03-18 23:45:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 35.52 ms, Memory: 67.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,10 @@
+const solution = (participant, completion) => {
+    const map = new Map();
+    for (let i = 0; i < participant.length; i++) {
+        map.set(participant[i], (map.get(participant[i]) || 0) + 1);
+        map.set(completion[i], (map.get(completion[i]) || 0) - 1);
+    }
+      for (const [name, value] of map) {
+        if (value > 0) return name;
+    }
+}
